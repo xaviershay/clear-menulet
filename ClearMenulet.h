96da9cbc59358465aa30f271ddd25548fd9a9551
Xavier Shay <xavier@rhnh.net>
2009-11-15 09:47:53 +1100

    Initial import

---
+++ ClearMenulet.h
@@ -0,0 +1,22 @@
+//
+//  ClearMenulet.h
+//  clear-menulet
+//
+//  Created by Xavier Shay on 15/11/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface ClearMenulet : NSObject {
+	IBOutlet NSMenu *statusMenu;
+	
+	NSStatusItem *statusItem;
+	NSImage *marketOpenImage;
+	NSImage *marketClosedImage;
+}
+
+-(IBAction)helloWorld:(id)sender;
+
+@end
---
+++ ClearMenulet.m
@@ -0,0 +1,49 @@
+//
+//  ClearMenulet.m
+//  clear-menulet
+//
+//  Created by Xavier Shay on 15/11/09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "ClearMenulet.h"
+
+
+@implementation ClearMenulet
+
+- (void) awakeFromNib{
+	
+	//Create the NSStatusBar and set its length
+	statusItem = [[[NSStatusBar systemStatusBar] statusItemWithLength:NSSquareStatusItemLength] retain];
+	
+	//Used to detect where our files are
+	NSBundle *bundle = [NSBundle mainBundle];
+	
+	//Allocates and loads the images into the application which will be used for our NSStatusItem
+	marketOpenImage   = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"clock" ofType:@"png"]];
+	marketClosedImage = [[NSImage alloc] initWithContentsOfFile:[bundle pathForResource:@"clock_red" ofType:@"png"]];
+	
+	//Sets the images in our NSStatusItem
+	[statusItem setImage:marketOpenImage];
+	//[statusItem setAlternateImage:statusHighlightImage];
+	
+	//Tells the NSStatusItem what menu to load
+	[statusItem setMenu:statusMenu];
+	//Sets the tooptip for our item
+	[statusItem setToolTip:@"My Custom Menu Item"];
+	//Enables highlighting
+	[statusItem setHighlightMode:YES];
+}
+
+- (void) dealloc {
+	//Releases the 2 images we loaded into memory
+	[marketOpenImage release];
+	[marketClosedImage release];
+	[super dealloc];
+}
+
+-(IBAction)helloWorld:(id)sender{
+	[statusItem setImage:marketClosedImage];
+	NSLog(@"Hello there!");
+}
+@end
